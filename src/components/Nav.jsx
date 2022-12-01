377f2b8459564513fdfe7646b4b5cddad7ca84f7
Luke Barker <luke.barker@student.swic.edu>
2022-11-30 20:21:15 -0600

    Replace "a" tags with Link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,14 @@
+
+import { Link } from "react-router-dom";
+
 export default function Nav(){
     return (<nav>
         <ul>
             <li>
-                <a href={'contacts/1'}>Your Name</a>
+                <Link href={'contacts/1'}>Your Name</Link>
             </li>
             <li>
-                <a href={'contacts/2'}>Your Friend</a>
+                <Link href={'contacts/2'}>Your Friend</Link>
             </li>
         </ul>
     </nav>
